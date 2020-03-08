17483a91e779cfebf0a43a4294bd21d84f6c7978
unviable <53972027+unviable@users.noreply.github.com>
2020-03-08 18:23:32 +0800

    sadasds

--- src/main/java/com/ali/main/MainApplication.java
+++ src/main/java/com/ali/main/MainApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
 
     public static void main(String[] args) {
+        System.out.println("11");
         SpringApplication.run(MainApplication.class, args);
     }
 
